037b419b34fd3bbc90a13b233c5177b75e5de150
OhRaekyu <dhforb1234@naver.com>
2021-12-31 23:39:37 +0900

    :memo: 파일수정(마크업 및 스타일 수정)

--- script/script.js
+++ script/script.js
@@ -1,45 +1,17 @@
-// 스크롤 애니메이션 (헤더)
-const headBar = document.querySelector(".head");
-
-let prevScrollTop = 0;
-document.addEventListener("scroll", () => { 
-    const nextScrollTop = window.pageYOffset || 0; 
-    if (nextScrollTop > prevScrollTop){
-        headBar.classList.add("scrollDown");
-    } else if (nextScrollTop < prevScrollTop){
-        headBar.classList.remove("scrollDown");
-    }
-    prevScrollTop = nextScrollTop;
-});
-
-// 상단 네비게이션 이벤트 처리
-const burgerNav = document.querySelector(".nav-trigger");
-const headNav = document.querySelector(".head-nav");
-const headNavEle = document.querySelectorAll(".head-nav ul li");
-
-burgerNav.addEventListener("click", () => {
-    burgerNav.classList.toggle("active");
-    headNav.classList.toggle("on");
-});
-
-headNavEle.forEach(ele => ele.addEventListener("click", () => {
-    burgerNav.classList.toggle("active");
-    headNav.classList.toggle("on");
-}
-))
-
 // 스크롤 애니메이션 (컨텐츠)
+const main = document.querySelector(".main");
 const saList = document.querySelectorAll(".sa");
 
 const scrollAnimation = function() {
-    for (const ele of saList) {
-        if (!ele.classList.contains("show")) {
-            console.log("debug #1");
-            if (window.innerHeight > ele.getBoundingClientRect().top + (window.innerHeight * 0.5)) {
-                ele.classList.add("show");
-                console.log("debug #2");
-            } 
-            // 스크롤 다시 올릴 때 처리해야함
+    for (let i = 0; i < saList.length; i++) {
+        if (!saList[i].classList.contains("show")) {
+            if ((window.innerHeight * 0.65) > saList[i].getBoundingClientRect().top) {
+                saList[i].classList.add("show");
+            }
+        } else {
+            if ((window.innerHeight * 0.65) < saList[i].getBoundingClientRect().top) {
+                saList[i].classList.remove("show");
+            }
         }
     }
 };
