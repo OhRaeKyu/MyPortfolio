b73303b521f6669c149c93c406720b29572ce55d
OhRaekyu <dhforb1234@naver.com>
2022-01-19 15:21:49 +0900

    :memo: 스타일 추가

--- script/script.js
+++ script/script.js
@@ -1,14 +1,29 @@
 const app = document.querySelector('#app');
-const saContents = document.querySelectorAll('.sa');
-const moduleTop = document.querySelectorAll('.main-module');
-const chagePoint = moduleTop[1].getBoundingClientRect().top;
-
-// 시작 화면 스크롤 애니메이션
+const moveTopBtn = document.querySelector('#moveTopBtn');
+const secStack = document.querySelector('#secStack');
 const balloonList = document.querySelector('.bg-balloon');
 const saStarts = document.querySelectorAll('.sa-start');
+const saContents = document.querySelectorAll('.sa');
+const moduleTop = document.querySelectorAll('.main-module');
+const chagePoint =
+  moduleTop[2].getBoundingClientRect().top - window.innerHeight / 2;
+
+// 상단 이동 버튼 클릭 이벤트
+moveTopBtn.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+
+// 시작 화면 스크롤 애니메이션
 const scrollAnimationStart = function () {
   const currentScroll = window.scrollY;
   const innerHeight = window.innerHeight;
+  // 상단으로 이동 버튼 숨김 처리
+  if (currentScroll > 0) {
+    moveTopBtn.classList.add('off');
+  } else {
+    moveTopBtn.classList.remove('off');
+  }
+
   // 풍선 애니메이션 숨김 처리
   if (currentScroll > innerHeight * 0.5) {
     balloonList.classList.add('off');
@@ -17,9 +32,9 @@ const scrollAnimationStart = function () {
   }
   // 로고 슬라이드 애니메이션
   if (currentScroll < innerHeight) {
-    saStarts[0].style.transform = `translateX(${currentScroll * 2}px)`;
+    saStarts[0].style.transform = `translateX(${currentScroll * 3}px)`;
     saStarts[0].style.opacity = 1;
-    saStarts[1].style.transform = `translateX(-${currentScroll * 2}px)`;
+    saStarts[1].style.transform = `translateX(-${currentScroll * 3}px)`;
     saStarts[1].style.opacity = 1;
   } else {
     saStarts[0].style.opacity = 0;
@@ -28,8 +43,12 @@ const scrollAnimationStart = function () {
   // 백그라운드 변경 애니메이션
   if (currentScroll > chagePoint) {
     app.classList.add('show');
+    moveTopBtn.classList.add('dark');
+    secStack.classList.add('dark');
   } else {
     app.classList.remove('show');
+    moveTopBtn.classList.remove('dark');
+    secStack.classList.remove('dark');
   }
 };
 
@@ -51,3 +70,8 @@ const scrollAnimationMain = function () {
 
 document.addEventListener('scroll', scrollAnimationStart);
 document.addEventListener('scroll', scrollAnimationMain);
+
+// 새로고침 시 최 상단으로
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0);
+};
